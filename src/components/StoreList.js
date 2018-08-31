b19364a8dd8c3aceebdde6bf820c4daea73f20ff
SHOKI TOMARU <ac.milan.22x36@gmail.com>
2018-08-31 19:39:48 +0900

    import axios

--- src/components/StoreList.js
+++ src/components/StoreList.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
+import axios from 'axios';
 import Header from './Header';
 import Store from './Store';
 
 class StoreList extends Component {
+  componentWillMount() {
+    axios.get('http://localhost:3000/dish')
+      .then(response => console.log(response));
+  }
+
   render() {
     return (
       <View>
