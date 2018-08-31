54dad8c72aad763599ddec7f58257b8eb38c4e8d
SHOKI TOMARU <ac.milan.22x36@gmail.com>
2018-08-31 16:34:38 +0900

    add  const Router

--- src/Router.js
+++ src/Router.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import { Router, Scene, Actions } from 'react-native-router-flux';
+
+const Router = () => {
+  return (
+    <Router>
+      <Scene>
+
+      </Scene>
+    </Router>
+  );
+};
+
+export default Router;
