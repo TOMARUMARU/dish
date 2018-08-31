707917f0d3adadd3272a17c1238d0cf1437cc7d1
SHOKI TOMARU <ac.milan.22x36@gmail.com>
2018-08-31 19:52:53 +0900

    renderStores

--- src/components/StoreList.js
+++ src/components/StoreList.js
@@ -1,20 +1,28 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import axios from 'axios';
 import Header from './Header';
 import Store from './Store';
 
 class StoreList extends Component {
+  state={ datas: [] };
+
   componentWillMount() {
     axios.get('http://localhost:3000/dish')
-      .then(response => console.log(response));
+      .then(response => this.setState({ datas: response.data }));
+  }
+
+  renderStores() {
+    return this.state.datas.map(data =>
+      <Store key={data.title} data={data} />
+    );
   }
 
   render() {
     return (
       <View>
         <Header />
-        <Store />
+        {this.renderStores()}
       </View>
     );
   }
