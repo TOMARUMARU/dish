56d0e7bccf5f82b9ac71d6d008e96504abf5735a
SHOKI TOMARU <ac.milan.22x36@gmail.com>
2018-08-31 20:35:55 +0900

    add card styles

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,11 +2,28 @@ import React from 'react';
 import { View } from 'react-native';
 
 const Card = (props) => {
+  const { containerStyle } = styles;
+
   return (
-    <View>
+    <View style={containerStyle}>
       {props.children}
     </View>
   );
+};
+
+const styles = {
+  containerStyle: {
+    height: 600,
+    borderWidth: 1,
+    borderRadius: 1,
+    borderColor: '#ddd',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    marginTop: 20,
+    marginBottom: 20,
+    marginLeft: 20,
+    marginRight: 20
+  }
 };
 
 export default Card;
