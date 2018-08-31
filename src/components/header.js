09f8d18b1dc937de27efea95893dc41410e3dd2f
SHOKI TOMARU <ac.milan.22x36@gmail.com>
2018-08-31 17:15:14 +0900

    add View & text style

--- src/components/header.js
+++ src/components/header.js
@@ -2,13 +2,32 @@ import React from 'react';
 import { View, Text } from 'react-native';
 
 const Header = () => {
+  const { headerStyle, mainTextStyle, subTextStyle } = styles;
+
   return (
-    <View>
-      <Text>
-        とまるまるまる
+    <View style={headerStyle}>
+      <Text style={mainTextStyle}>
+        気になるお店を右フリック！
+      </Text>
+      <Text style={subTextStyle}>
+        残り890件
       </Text>
     </View>
   );
+};
+
+const styles = {
+  headerStyle: {
+    marginTop: 60,
+    alignItems: 'center'
+  },
+  mainTextStyle: {
+    fontSize: 15
+  },
+  subTextStyle: {
+    fontSize: 12,
+    opacity: 0.7
+  }
 };
 
 export default Header;
