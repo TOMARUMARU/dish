3d5d3d68a8aa4412d17a9aff7ee66c3df5ef796d
SHOKI TOMARU <ac.milan.22x36@gmail.com>
2018-08-31 20:59:48 +0900

    add card style

--- src/components/Store.js
+++ src/components/Store.js
@@ -3,19 +3,33 @@ import { Text, Image } from 'react-native';
 import Card from './Card';
 
 const Store = ({ data }) => {
+  const { imageStyle, titleStyle } = styles;
+
   const { title, image } = data;
 
   return (
     <Card>
       <Image
         source={{ uri: image }}
-        style={{ height: 450, width: 333 }}
+        style={imageStyle}
       />
-      <Text>
+      <Text style={titleStyle}>
         {title}
       </Text>
     </Card>
   );
+};
+
+const styles = {
+  imageStyle: {
+    height: 450,
+    width: 333
+  },
+  titleStyle: {
+    fontSize: 30,
+    marginTop: 10,
+    marginLeft: 10
+  }
 };
 
 export default Store;
