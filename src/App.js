c712d6c5051cd78245f78ff8fe451b915b806b11
SHOKI TOMARU <ac.milan.22x36@gmail.com>
2018-08-31 16:22:18 +0900

    import Header

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import Header from './components/header';
 
 class App extends Component {
   render() {
     return (
       <Provider store={createStore}>
-        <View>
-          <Text>
-            とまるまるまる
-          </Text>
-        </View>
+        <Header />
       </Provider>
     );
   }
