520a31fe85b8f4c61e2d45db8a24d1bc233c60ca
SHOKI TOMARU <ac.milan.22x36@gmail.com>
2018-08-31 17:23:25 +0900

    import Icon from fether

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 
 const Header = () => {
   const { headerStyle, mainTextStyle, subTextStyle } = styles;
