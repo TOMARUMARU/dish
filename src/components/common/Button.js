950fd1d7f6eed8d772a2badbbaeac62291b70c83
SHOKI TOMARU <ac.milan.22x36@gmail.com>
2018-08-31 22:32:00 +0900

    add buttonStyle

--- src/components/common/Button.js
+++ src/components/common/Button.js
@@ -1,12 +1,37 @@
 import React from 'react';
+import { View } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
 
-const Button = ({ name }) => {
+const Button = ({ name, color }) => {
+  const { buttonStyle } = styles;
+
   return (
-    <Icon
-      name={name}
-    />
+    <View style={buttonStyle}>
+      <Icon
+        name={name}
+        style={{ color, fontSize: 20 }}
+      />
+    </View>
   );
+};
+
+
+const styles = {
+  buttonStyle: {
+    marginTop: 5,
+    marginRight: 40,
+    height: 50,
+    width: 50,
+    borderWidth: 1,
+    borderRadius: 100,
+    borderColor: '#ddd',
+    shadowColor: '#000',
+    shadowOffset: { wiedh: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 2,
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
 };
 
 export { Button };
