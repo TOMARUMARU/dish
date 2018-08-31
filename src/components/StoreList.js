c9edd01dfb8b0e916271a72c9517d8b09475dabc
SHOKI TOMARU <ac.milan.22x36@gmail.com>
2018-08-31 18:22:51 +0900

    create StoreList Component

---
+++ src/components/StoreList.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Header from './Header';
+
+class StoreList extends Component {
+  render() {
+    return (
+      <Header />
+    );
+  }
+}
+
+export default StoreList;
