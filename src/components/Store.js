693b67d5d085b9e4f5e9f059e9ebfadc9f052788
SHOKI TOMARU <ac.milan.22x36@gmail.com>
2018-08-31 19:11:31 +0900

    add Store.js

---
+++ src/components/Store.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Store = () => {
+  return (
+    <View>
+      <Text>
+        aaaaa
+      </Text>
+    </View>
+  );
+};
+
+export default Store;
--- src/components/StoreList.js
+++ src/components/StoreList.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import { View } from 'react-native';
 import Header from './Header';
+import Store from './Store';
 
 class StoreList extends Component {
   render() {
     return (
-      <Header />
+      <View>
+        <Header />
+        <Store />
+      </View>
     );
   }
 }
