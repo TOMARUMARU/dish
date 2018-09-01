4a1419fe7a4bbe62e289b572aae10d0ba55f0845
SHOKI TOMARU <ac.milan.22x36@gmail.com>
2018-09-01 17:09:49 +0900

    import reducers

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import reducers from './reducers';
 import Router from './Router';
 
 class App extends Component {
   render() {
     return (
-      <Provider store={createStore}>
+      <Provider store={createStore(reducers)}>
         <Router />
       </Provider>
     );
