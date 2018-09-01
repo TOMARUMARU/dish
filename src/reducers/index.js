c7a4fc7868b776afa8d0cd8b6d1eaf9fd00308e8
SHOKI TOMARU <ac.milan.22x36@gmail.com>
2018-09-01 23:00:50 +0900

    fix name

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,5 +2,5 @@ import { combineReducers } from 'redux';
 import DisplayedReducer from './DisplayedReducer';
 
 export default combineReducers({
-  display: DisplayedReducer
+  displayedCardId: DisplayedReducer
 });
