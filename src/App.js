6a2d84c0ffab06edee1e2e43829a8465de76682a
SHOKI TOMARU <ac.milan.22x36@gmail.com>
2018-09-01 16:12:48 +0900

    一旦redux消す

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import React, { Component } from 'react';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
 import Router from './Router';
 
 class App extends Component {
   render() {
     return (
-      <Provider store={createStore}>
-        <Router />
-      </Provider>
+      <Router />
     );
   }
 }
