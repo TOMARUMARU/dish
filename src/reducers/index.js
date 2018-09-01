47b69bc3d0f8f8663ca9f0a71eb06b05a093b945
SHOKI TOMARU <ac.milan.22x36@gmail.com>
2018-09-01 20:05:53 +0900

    add index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,6 @@
 import { combineReducers } from 'redux';
+import FavoriteReducer from './FavoriteReducer';
 
 export default combineReducers({
-  tomarus: () => []
+  favorites: FavoriteReducer
 });
