b7836fb0d1ed8cbde921319c01dd1bb666f68869
SHOKI TOMARU <ac.milan.22x36@gmail.com>
2018-09-03 15:56:06 +0900

    add debugger

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import { createStore } from 'redux';
 import reducers from './reducers';
 import Router from './Router';
 
+const store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
 class App extends Component {
   render() {
     return (
-      <Provider store={createStore(reducers)}>
+      <Provider store={store}>
         <Router />
       </Provider>
     );
