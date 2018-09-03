91a134c0afba6b17d6a73e35dd7ca25a80a6af1f
SHOKI TOMARU <ac.milan.22x36@gmail.com>
2018-09-03 20:36:11 +0900

    create FavoriteList component

---
+++ src/components/FavoriteList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class FavoriteList extends Component {
+  render() {
+    return (
+      <View>
+        <Text>hoge</Text>
+      </View>
+    );
+  }
+}
+
+export default FavoriteList;
