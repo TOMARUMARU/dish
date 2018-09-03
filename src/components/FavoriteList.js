9f9bdea6156ae0821fff1114f7be10dd95ffb261
SHOKI TOMARU <ac.milan.22x36@gmail.com>
2018-09-03 21:40:50 +0900

    create List.js

--- src/components/FavoriteList.js
+++ src/components/FavoriteList.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { List } from './common';
 
 class FavoriteList extends Component {
   render() {
     return (
-      <View>
-        <Text>hoge</Text>
-      </View>
+      <List>
+        {this.props.children}
+      </List>
     );
   }
 }
