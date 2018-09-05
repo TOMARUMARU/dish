0c3678fb5e26b9102daae4605f71756b8e8c69a7
SHOKI TOMARU <ac.milan.22x36@gmail.com>
2018-09-05 21:04:16 +0900

    Router typescript

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,3 +1,4 @@
+import * as React from 'react'
 import React from 'react';
 import { Router, Scene, Modal } from 'react-native-router-flux';
 import StoreList from './screens/StoreList/screen.component';
