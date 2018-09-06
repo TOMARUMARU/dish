724cc6b3aa622c200cd59bf4346675e07cd01c0b
SHOKI TOMARU <ac.milan.22x36@gmail.com>
2018-09-06 16:01:24 +0900

    reducer.ts prettier

--- src/reducer.ts
+++ src/reducer.ts
@@ -1,6 +1,6 @@
-import { combineReducers } from 'redux';
-import AllCardsReducer from './screens/StoreList/reducer';
-import FavoriteReducer from './screens/FavoriteList/reducer';
+import { combineReducers } from "redux";
+import AllCardsReducer from "./screens/StoreList/reducer";
+import FavoriteReducer from "./screens/FavoriteList/reducer";
 
 export default combineReducers({
   allCards: AllCardsReducer,
