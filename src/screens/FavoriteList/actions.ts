9ef2003c4698493845be1ff5a58f30390bcd070e
SHOKI TOMARU <ac.milan.22x36@gmail.com>
2018-09-06 20:12:09 +0900

    change integer -> number

--- src/screens/FavoriteList/actions.ts
+++ src/screens/FavoriteList/actions.ts
@@ -1,6 +1,6 @@
 import { FAVORITE_CARDS } from '../../actions/types';
 
-export const favoriteCards = (cardId: 'integer') => {
+export const favoriteCards = (cardId: number) => {
   return {
     type: FAVORITE_CARDS,
     payload: cardId
