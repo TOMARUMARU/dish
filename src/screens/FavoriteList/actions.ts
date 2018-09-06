ae754734a0faa2292ade1ae5498bea6b3d7bad8d
SHOKI TOMARU <ac.milan.22x36@gmail.com>
2018-09-06 19:05:49 +0900

    FavoriteList actions.ts

--- src/screens/FavoriteList/actions.ts
+++ src/screens/FavoriteList/actions.ts
@@ -1,6 +1,6 @@
 import { FAVORITE_CARDS } from '../../actions/types';
 
-export const favoriteCards = cardId => {
+export const favoriteCards = (cardId: 'integer') => {
   return {
     type: FAVORITE_CARDS,
     payload: cardId
