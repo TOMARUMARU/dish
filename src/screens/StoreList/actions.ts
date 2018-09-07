8198f965535c61d5351678c6f4af9d6508fcf01e
SHOKI TOMARU <ac.milan.22x36@gmail.com>
2018-09-07 22:32:04 +0900

    delete import

--- src/screens/StoreList/actions.ts
+++ src/screens/StoreList/actions.ts
@@ -1,7 +1,3 @@
-import actionCreatorFactory from 'typescript-fsa';
-import axios from 'axios';
-import { DEFAULT_CARDS } from '../../actions/types';
-
 export const fetchDefaultCards = () => {
   return { type: 'FETCH_DEFAULT_CARDS' };
 };
