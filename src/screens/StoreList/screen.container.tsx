ebff69a4db2c08403fd225ff491e102b450ee011
SHOKI TOMARU <ac.milan.22x36@gmail.com>
2018-09-07 20:23:50 +0900

    separate component, container

---
+++ src/screens/StoreList/screen.container.tsx
@@ -0,0 +1,25 @@
+import { connect } from 'react-redux';
+import { Dispatch, Action } from 'redux';
+import { RootState } from '../../reducer';
+import { fetchDefaultCards, favoriteCards } from '../../actions';
+import Component, { StateProps, DispatchProp } from './screen.component';
+
+const mapStateToProps = (state: RootState): StateProps => {
+  const { isRequesting } = state.screens.storeList;
+  const datas = null;
+  // const favorites = state.favoriteCardIds;
+
+  return { datas, isRequesting };
+};
+
+const mapDispatchToProps = (dispatch: Dispatch<Action>): DispatchProp => {
+  return {
+    onFetchDefaultCards: () => dispatch(fetchDefaultCards())
+    // favoriteCards: () => dispatch(favoriteCards())
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Component);
