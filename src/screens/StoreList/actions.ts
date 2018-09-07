ba1dcebb43f65c7195e9357bb59076b87bb030c9
SHOKI TOMARU <ac.milan.22x36@gmail.com>
2018-09-07 20:23:07 +0900

    create fetchDefaultCards

--- src/screens/StoreList/actions.ts
+++ src/screens/StoreList/actions.ts
@@ -2,17 +2,27 @@ import actionCreatorFactory from 'typescript-fsa';
 import axios from 'axios';
 import { DEFAULT_CARDS } from '../../actions/types';
 
-export const defaultCards = () => {
-  return (dispatch: Function) => {
-    axios
-      .get('http://localhost:3000/dish')
-      .then(response => DefalutData(dispatch, response));
-  };
-};
+// const actionCreator = actionCreatorFactory();
 
-const DefalutData = (dispatch: Function, datas: {}) => {
-  dispatch({
-    type: DEFAULT_CARDS,
-    payload: datas
-  });
+// export const defaultCards = {
+//   submit: {} = actionCreator<{datas: {}}>(DEFAULT_CARDS)
+// }
+
+// export const defaultCards = () => {
+//   return (dispatch: Function) => {
+//     axios
+//       .get('http://localhost:3000/dish')
+//       .then(response => DefalutData(dispatch, response));
+//   };
+// };
+
+// const DefalutData = (dispatch: Function, datas: {}) => {
+//   dispatch({
+//     type: DEFAULT_CARDS,
+//     payload: datas
+//   });
+// };
+
+export const fetchDefaultCards = () => {
+  return { type: 'FETCH_DEFAULT_CARDS' };
 };
