eeb770907627e5da30caaaa81157029524ed3c8b
SHOKI TOMARU <ac.milan.22x36@gmail.com>
2018-09-07 22:07:49 +0900

    ajax in epic

--- src/screens/StoreList/epic.ts
+++ src/screens/StoreList/epic.ts
@@ -1,12 +1,18 @@
 import { combineEpics, Epic } from 'redux-observable';
-import { filter, tap, ignoreElements } from 'rxjs/operators';
+import { filter, mergeMap, map, tap, ignoreElements } from 'rxjs/operators';
 import { Action } from 'redux';
+import { ajax } from 'rxjs/ajax';
 
 export const fetchDefaultEpic: Epic<Action> = action$ =>
   action$.pipe(
     filter(action => action.type === 'FETCH_DEFAULT_CARDS'),
-    tap(() => console.log('aaaaa')),
-    ignoreElements()
+    mergeMap(action =>
+      ajax
+        .getJSON('http://localhost:3000/dish')
+        .pipe(
+          map(response => ({ type: 'FETCH_CARD_SUCCESS', payload: response }))
+        )
+    )
   );
 
 export default combineEpics(fetchDefaultEpic);
