79854888e760aeedad51d917489e66634e4c315e
SHOKI TOMARU <ac.milan.22x36@gmail.com>
2018-09-07 22:11:42 +0900

    add datas to reducer

--- src/screens/StoreList/reducer.ts
+++ src/screens/StoreList/reducer.ts
@@ -2,14 +2,17 @@ import { Action } from 'redux';
 
 export interface State {
   isRequesting: boolean;
+  datas: {};
 }
 
-const INITIAL_STATE: State = { isRequesting: false };
+const INITIAL_STATE: State = { isRequesting: false, datas: '' };
 
 export default (state = INITIAL_STATE, action: Action) => {
   switch (action.type) {
     case 'FETCH_DEFAULT_CARDS':
-      return { isRequesting: true };
+      return { ...state, isRequesting: true };
+    case 'FETCH_CARD_SUCCESS':
+      return { ...state, isRequesting: false, datas: action.payload };
     default:
       return state;
   }
