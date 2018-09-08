46fbc26b78d14e5641f173b37fe7c182c1f7c2b8
SHOKI TOMARU <ac.milan.22x36@gmail.com>
2018-09-08 21:56:53 +0900

    delete src/actions

--- src/screens/StoreList/screen.container.tsx
+++ src/screens/StoreList/screen.container.tsx
@@ -1,14 +1,23 @@
 import { connect } from 'react-redux';
 import { Dispatch, Action } from 'redux';
+import { denormalize } from 'normalizr';
 import { RootState } from '../../reducer';
-import { defaultCardsActions, favoriteActions } from '../../action';
+import { favoriteActions } from '../FavoriteList/actions';
+import { defaultCardsActions } from '../../entities/cards/action';
 import Component, { StateProps, DispatchProp } from './screen.component';
+import { cardsSchema } from '../../entities/cards/schema';
+import { Card } from '../../entities';
 
 const mapStateToProps = (state: RootState): StateProps => {
-  const { isRequesting, datas } = state.screens.storeList;
+  const { isRequesting } = state.screens.storeList;
+  const cards: Array<Card | undefined> = denormalize(
+    Object.keys(state.entities.cards),
+    cardsSchema,
+    state.entities
+  );
   const { favorites } = state.screens.favoriteList;
 
-  return { isRequesting, datas, favorites };
+  return { isRequesting, cards, favorites };
 };
 
 const mapDispatchToProps = (dispatch: Dispatch<Action>): DispatchProp => {
