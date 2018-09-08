d75dbcc068c3d56eb5bcf5531cd2c5e85b3dc39c
SHOKI TOMARU <ac.milan.22x36@gmail.com>
2018-09-08 16:24:58 +0900

    import Card & add type

--- src/screens/StoreList/actions.ts
+++ src/screens/StoreList/actions.ts
@@ -1,8 +1,9 @@
 import actionCreatorFactory from 'typescript-fsa';
+import { Card } from '../../entities';
 const defaultCardsActionCreator = actionCreatorFactory('Screens/StoreList');
 
 export const defaultCardsActions = {
   fetch: defaultCardsActionCreator('FETCH_DEFAULT_CARDS'),
-  success: defaultCardsActionCreator<{}>('FETCH_CARD_SUCCESS'),
+  success: defaultCardsActionCreator<Card[]>('FETCH_CARD_SUCCESS'),
   failure: defaultCardsActionCreator<string>('FETCH_CARD_FAILURE')
 };
