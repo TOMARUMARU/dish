247447601e22e762bc418d2119ed669ea415869d
SHOKI TOMARU <ac.milan.22x36@gmail.com>
2018-09-08 21:57:09 +0900

    delete StoreList/actions & epics

--- src/screens/StoreList/actions.ts
+++
@@ -1,9 +0,0 @@
-import actionCreatorFactory from 'typescript-fsa';
-import { Card } from '../../entities';
-const defaultCardsActionCreator = actionCreatorFactory('Screens/StoreList');
-
-export const defaultCardsActions = {
-  fetch: defaultCardsActionCreator('FETCH_DEFAULT_CARDS'),
-  success: defaultCardsActionCreator<Card[]>('FETCH_CARD_SUCCESS'),
-  failure: defaultCardsActionCreator<string>('FETCH_CARD_FAILURE')
-};
