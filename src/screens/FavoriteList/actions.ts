1d3796d92835dea047351245e4489d88307a1330
SHOKI TOMARU <ac.milan.22x36@gmail.com>
2018-09-08 15:06:19 +0900

    add fetch to favoriteActions

--- src/screens/FavoriteList/actions.ts
+++ src/screens/FavoriteList/actions.ts
@@ -2,5 +2,6 @@ import actionCreatorFactory from 'typescript-fsa';
 const favoriteActionCreator = actionCreatorFactory('screens/FavoriteList');
 
 export const favoriteActions = {
-  add: favoriteActionCreator<number>('ADD_FAVORITE_CARDS')
+  add: favoriteActionCreator<number>('ADD_FAVORITE_CARDS'),
+  fetch: favoriteActionCreator('FETCH_FAVORITE_CARDS')
 };
