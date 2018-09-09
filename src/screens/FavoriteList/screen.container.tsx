32b6bce8f5a74f01686374ce02e8594abcb1ef30
SHOKI TOMARU <ac.milan.22x36@gmail.com>
2018-09-09 21:12:09 +0900

    FavoriteList container fix

--- src/screens/FavoriteList/screen.container.tsx
+++ src/screens/FavoriteList/screen.container.tsx
@@ -1,12 +1,17 @@
 import { connect } from 'react-redux';
 import { RootState } from '../../reducer';
 import Component, { StateProps } from './screen.component';
+import { cardsSchema } from '../../entities/cards/schema';
+import { Card } from '../../entities';
+import { denormalize } from 'normalizr';
 
 const mapStateToProps = (state: RootState): StateProps => {
-  const { datas } = state.screens.storeList;
-  const { favorites } = state.screens.favoriteList;
-
-  return { datas, favorites };
+  const cards: Array<Card | undefined> = denormalize(
+    Object.keys(state.entities.cards),
+    cardsSchema,
+    state.entities
+  );
+  return { cards };
 };
 
 export default connect(mapStateToProps)(Component);
