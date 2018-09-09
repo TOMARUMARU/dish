cf381ccf0cccc57add2f760ea4146435236fa333
SHOKI TOMARU <ac.milan.22x36@gmail.com>
2018-09-09 20:11:55 +0900

    delete FavoriteLidstReducer

--- src/screens/reducer.ts
+++ src/screens/reducer.ts
@@ -1,15 +1,10 @@
 import { combineReducers } from 'redux';
 import StoreListReducer, { State as StoreListState } from './StoreList/reducer';
-import FavoriteListReducer, {
-  State as FavorieListState
-} from './FavoriteList/reducer';
 
 export interface ScreensState {
   storeList: StoreListState;
-  favoriteList: FavorieListState;
 }
 
 export default combineReducers<ScreensState>({
-  storeList: StoreListReducer,
-  favoriteList: FavoriteListReducer
+  storeList: StoreListReducer
 });
