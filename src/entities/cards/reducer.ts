a03e8da853d21704b6bf4b6bb8fb0c382aa74282
SHOKI TOMARU <ac.milan.22x36@gmail.com>
2018-09-10 19:04:13 +0900

    change reducer state

--- src/entities/cards/reducer.ts
+++ src/entities/cards/reducer.ts
@@ -3,16 +3,18 @@ import { CardsActions } from './action';
 import { Card } from '../../entities';
 
 export interface State {
-  [id: number]: Card;
+  cards: {
+    [id: number]: Card;
+  };
 }
 
-const INITIAL_STATE: State = {};
+const INITIAL_STATE: State = { cards: {} };
 
 export default reducerWithInitialState(INITIAL_STATE)
   .case(CardsActions.success, (state, payload) => {
-    return { ...state, ...payload.entities.cards };
+    return { ...state, ...payload.entities };
   })
   .case(CardsActions.favorite, (state, payload) => {
-    state[payload].favorited = true;
+    state.cards[payload].favorited = true;
     return { ...state };
   });
